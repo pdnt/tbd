a29d3428e5481bfdd9a5cee81e8fb970bb558697
pdnt <pdntspa@protonmail.ch>
2020-07-01 19:29:48 -0300

    Add test for Parse text

--- linter/dups.go
+++ linter/dups.go
@@ -1,18 +1,15 @@
 package linter
 
-import (
-	"fmt"
-	"regexp"
-)
+import "fmt"
 
 func FindDups(text string) []string {
-	wordFinderRegex := regexp.MustCompile(`\W+`)
-	words := wordFinderRegex.Split(text, -1)
+	// wordFinderRegex := regexp.MustCompile(`\W+`)
+	// words := wordFinderRegex.Split(text, -1)
 	out := []string{}
-
-	for _, word := range words {
-		fmt.Printf("this is a word: %s\n", word)
-	}
+	fmt.Println(Parse(text))
+	// for _, word := range words {
+	// 	fmt.Printf("this is a word: %s\n", word)
+	// }
 
 	return out
 }
