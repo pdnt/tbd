699b1c9a9fb4e98f95d99bc746e9dc2a44b1e7f0
pdnt <pdntspa@protonmail.ch>
2020-07-01 19:34:42 -0300

    Add comments

--- linter/parser.go
+++ linter/parser.go
@@ -10,6 +10,9 @@ type Token struct {
 	Row  int
 }
 
+// TODO
+//Fix line and word output values
+
 func Parse(text string) []Token {
 	tokens := []Token{}
 	line := 0
@@ -17,16 +20,16 @@ func Parse(text string) []Token {
 	row := 0
 
 	for _, code := range text {
-		// if newline, increment line
+		// If newline, increment line and resets row value. Else increments row value.
 		if code == 10 {
 			line++
 			row = 0
 		}
-
+		// If new letter, add letter to word.
 		if unicode.IsLetter(code) || unicode.IsSymbol(code) || unicode.IsPunct(code) {
 			word = word + string(code)
 		}
-
+		// If spacebreak, create token and allocate the word.
 		if unicode.IsSpace(code) {
 			token := &Token{
 				Word: word,
