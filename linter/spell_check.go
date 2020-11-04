6aa796a35f77095db77c3b8b1f0b54a9f4c4e955
pdnt <pdntspa@protonmail.ch>
2020-11-04 20:23:23 -0300

    fix: Stop reporting numbers as misspells

--- linter/spell_check.go
+++ linter/spell_check.go
@@ -1,11 +1,18 @@
 package linter
 
+import (
+	"regexp"
+)
+
 func FindMisspell(parser *Parser) []Token {
 	misspellings := []Token{}
 	allTokens := parser.GetTokens()
+	// FIXME: we should really check if a string is a number in the parser, but we decided to do this quick
+	// fix for the time being until we find another use-case that justify the effort
+	r, _ := regexp.Compile("^[0-9]+$")
 
 	for _, token := range allTokens {
-		if token.Kind == WordKind && !DictionaryWords.Has(&token) {
+		if !r.MatchString(token.Value) && token.Kind == WordKind && !DictionaryWords.Has(&token) {
 			misspellings = append(misspellings, token)
 		}
 	}
